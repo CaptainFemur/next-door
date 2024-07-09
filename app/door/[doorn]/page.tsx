436d9700498df84e28fc9945dc4cf9cad178cf19
CaptainFemur <jeremy.alves.neto@gmail.com>
2024-07-09 15:20:10 +0200

    OK (mais bug cache sur lien autre porte)

--- app/door/[doorn]/page.tsx
+++ app/door/[doorn]/page.tsx
@@ -1,6 +1,7 @@
 import { generateNumero } from "@/utils/randomNumero";
 import Image from "next/image";
 import Link from 'next/link'
+import Pokemon from "./pokemon";
 
 export async function generateMetadata({ params }: { params: { doorn: number } }) {
   return {
@@ -8,80 +9,7 @@ export async function generateMetadata({ params }: { params: { doorn: number } }
   }
 }
 
-function searchEvolution(chain: any, name: string, preName: string){
-  const tempName = chain?.species.name;
-  if(tempName === name){
-    return searchEvolution(chain.evolves_to[0], name, preName);
-  }
-  return preName === tempName ? false : chain?.species.url;
-}
-
-async function getPokemon(numero: number) {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+numero);
-
-  if(!response.ok){
-    throw new Error('Failed to fetch pokemon');
-  }
-
-  const pokemonDatas = await response.json();
-  const otherResponse = await fetch(pokemonDatas.species.url);
-
-  if(!otherResponse.ok){
-    throw new Error('Failed to fetch pokemon\'s datas');
-  }
-
-  const otherDatas = await otherResponse.json();
-
-  const preEvolution = otherDatas.evolves_from_species?.url;
-  let preEvolutionName = '';
-  if(preEvolution){
-    const preResponse = await fetch(preEvolution);
-
-    if(!preResponse.ok){
-      throw new Error('Failed to fetch pre-evo\'s datas');
-    }
-
-    const preDatas = await preResponse.json();
-    const preEvolutionNumero = preDatas.id;
-    preEvolutionName = preDatas.name;
-    pokemonDatas.preEvolution = preEvolutionNumero;
-  }
-
-  const evolution = otherDatas.evolution_chain?.url;
-  if(evolution){
-    const chainResponse = await fetch(evolution);
-
-    if(!chainResponse.ok){
-      throw new Error('Failed to fetch chain\'s evolution datas');
-    }
-
-    const chainDatas = await chainResponse.json();
-
-    const hasEvo = searchEvolution(chainDatas.chain.evolves_to[0], pokemonDatas.name, preEvolutionName);
-    if(hasEvo){
-      const evolutionResponse = await fetch(hasEvo);
-
-      if(!evolutionResponse.ok){
-        throw new Error('Failed to fetch evolution\'s datas');
-      }
-
-      const evolutionDatas = await evolutionResponse.json();
-      const evolutionNumero = evolutionDatas.id;
-      pokemonDatas.evolution = evolutionNumero;
-    }
-    
-  }
-
-  const nameInFrench = otherDatas.names.find((item: { language: { name: string } }) => item.language.name === 'fr')?.name;
-  pokemonDatas.name = nameInFrench || pokemonDatas.name;
-
-  return pokemonDatas;
-}
-
 export default async function DoorNumber({ params }: { params: { doorn: number } }) {
-  const pokemon = await getPokemon(params.doorn);
-  // console.log(pokemon);
-
   return (
     <main className="flex min-h-screen flex-col items-center py-12 px-24">
       <Link className="absolute left-20 top-20 text-start self-start bg-secondary text-white py-2 px-4 rounded hover:bg-transparent hover:text-secondary" href="/door">Retour</Link>
@@ -113,12 +41,7 @@ export default async function DoorNumber({ params }: { params: { doorn: number }
 
       <p className="py-12 text-4xl font-bold text-center lg:text-6xl">This is the door n°{params.doorn}</p>
 
-      <p className="text-xl text-center lg:text-2xl">Vous avez trouvé un <span className="text-primary font-bold capitalize">{pokemon.name}</span></p>
-      { pokemon.sprites && <Image src={pokemon.sprites.front_default} alt={pokemon.name} width={120} height={120} />}
-      <div className="flex gap-4">
-        {pokemon.preEvolution && <Link className="text-start self-start border-red-600 bg-red-600 text-white py-2 px-4 rounded hover:bg-transparent border-2 hover:text-red-600  hover:border-red-600" href={"/door/"+pokemon.preEvolution}>Désévoluer</Link> }
-        {pokemon.evolution && <Link className="text-start self-start border-green-600 bg-green-600 text-white py-2 px-4 rounded hover:bg-transparent border-2 hover:text-green-600 hover:border-green-600" href={"/door/"+pokemon.evolution}>Évoluer</Link>}
-      </div>
+      <Pokemon numero={params.doorn} />
     </main>
   );
 }
---
+++ app/door/[doorn]/pokemon.tsx
@@ -0,0 +1,89 @@
+import Image from "next/image";
+import Link from 'next/link'
+import { Suspense } from "react";
+
+
+function searchEvolution(chain: any, name: string, preName: string){
+  const tempName = chain?.species.name;
+  if(tempName === name){
+    return searchEvolution(chain.evolves_to[0], name, preName);
+  }
+  return preName === tempName ? false : chain?.species.url;
+}
+
+async function getPokemon(numero: number) {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+numero);
+
+  if(!response.ok){
+    throw new Error('Failed to fetch pokemon');
+  }
+
+  const pokemonDatas = await response.json();
+  const otherResponse = await fetch(pokemonDatas.species.url);
+
+  if(!otherResponse.ok){
+    throw new Error('Failed to fetch pokemon\'s datas');
+  }
+
+  const otherDatas = await otherResponse.json();
+
+  const preEvolution = otherDatas.evolves_from_species?.url;
+  let preEvolutionName = '';
+  if(preEvolution){
+    const preResponse = await fetch(preEvolution);
+
+    if(!preResponse.ok){
+      throw new Error('Failed to fetch pre-evo\'s datas');
+    }
+
+    const preDatas = await preResponse.json();
+    const preEvolutionNumero = preDatas.id;
+    preEvolutionName = preDatas.name;
+    pokemonDatas.preEvolution = preEvolutionNumero;
+  }
+
+  const evolution = otherDatas.evolution_chain?.url;
+  if(evolution){
+    const chainResponse = await fetch(evolution);
+
+    if(!chainResponse.ok){
+      throw new Error('Failed to fetch chain\'s evolution datas');
+    }
+
+    const chainDatas = await chainResponse.json();
+
+    const hasEvo = searchEvolution(chainDatas.chain.evolves_to[0], pokemonDatas.name, preEvolutionName);
+    if(hasEvo){
+      const evolutionResponse = await fetch(hasEvo);
+
+      if(!evolutionResponse.ok){
+        throw new Error('Failed to fetch evolution\'s datas');
+      }
+
+      const evolutionDatas = await evolutionResponse.json();
+      const evolutionNumero = evolutionDatas.id;
+      pokemonDatas.evolution = evolutionNumero;
+    }
+    
+  }
+
+  const nameInFrench = otherDatas.names.find((item: { language: { name: string } }) => item.language.name === 'fr')?.name;
+  pokemonDatas.name = nameInFrench || pokemonDatas.name;
+
+  return pokemonDatas;
+}
+
+export default async function Pokemon( params: { numero: number } ) {
+  const pokemon = await getPokemon(params.numero);
+
+  return (
+    <Suspense fallback={<p>Chargement...</p>}>
+      <p className="text-xl text-center lg:text-2xl">Vous avez trouvé un <span className="text-primary font-bold capitalize">{pokemon.name}</span></p>
+      <Image src={pokemon.sprites.front_default} alt={pokemon.name} width={120} height={120} />
+      <div className="flex gap-4">
+          {pokemon.preEvolution && <Link className="text-start self-start border-red-600 bg-red-600 text-white py-2 px-4 rounded hover:bg-transparent border-2 hover:text-red-600  hover:border-red-600" href={"/door/"+pokemon.preEvolution}>Désévoluer</Link> }
+          {pokemon.evolution && <Link className="text-start self-start border-green-600 bg-green-600 text-white py-2 px-4 rounded hover:bg-transparent border-2 hover:text-green-600 hover:border-green-600" href={"/door/"+pokemon.evolution}>Évoluer</Link>}
+      </div>
+    </Suspense>
+  );
+}
